597df423baeb6da3371dcc27de9f514d3d90e763
zhouxf14 <zhouxf14@163.com>
2018-10-01 11:49:05 -0400

    lab6 part1

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -6,3 +6,32 @@ function addNewList() {
 function addListItem() {
     console.log('hello world');
 }
+
+
+// $(document).ready(function(){
+//     $("#add-item").click(function() { // bind handler for click event
+        
+//         var list = $("#grocery-list"); // get the ol list by id
+//         var itemInput = $("#new-list-item"); // get the new item input
+// 	  // append the input value within an li element
+//         list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>"); 
+//     });
+
+//     $(".delete-item").click(function() {
+//     	console.log("test");
+// 	    $(this).parent().remove();
+// 	    console.log("test");
+// 	});
+
+// });
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
